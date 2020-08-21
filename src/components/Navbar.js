98cf9f6babe8795d64e99c7010ba0a79b5478b84
larapollehn <lara.pollehn@googlemail.com>
2020-08-21 14:22:05 +0200

    Basic components, with routing and styling of active/non-active tabs of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {Link, useLocation} from "react-router-dom";
-import log from "../log/Logger";
 
 
 const Navbar = () => {
