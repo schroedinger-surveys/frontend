66300e1ba3904eefa085a9fe5fe3b9d89bbc3054
larapollehn <lara.pollehn@googlemail.com>
2020-08-23 17:53:48 +0200

    Build left side of CreateSurvey, needs input validation

--- src/components/survey/CreateSurvey.js
+++ src/components/survey/CreateSurvey.js
@@ -1,10 +1,93 @@
-import React from "react";
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 const CreateSurvey = () => {
-    return(
-        <div>
-            <h1>Hello this is: CREATE SURVEY</h1>
-        </div>
+    const [questions, setQuestions] = useState([]);
+    const [values, setValues] = useState({
+        title: "",
+        description: "",
+        start_date: Date.now(),
+        end_date: Date.now() + 800,
+        secured: true
+    });
+    const {title, description, secured, start_date, end_date} = values;
+
+    const handleInputChange = (name) => (event) => {
+        setValues({...values, [name]: event.target.value})
+    }
+
+    const createSurvey = (event) => {
+        event.preventDefault();
+    }
+
+    const basicDataFormInput = () => {
+        return(
+            <Form style={{width: "70%", margin: "0 auto"}}>
+                <Form.Group controlId="surveyTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" value={title} onChange={handleInputChange("title")}/>
+                    <Form.Text className="text-muted">
+                        Public surveys are easier to find with a poignant title.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="surveyDescription">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows="3" placeholder="Description" value={description} onChange={handleInputChange("description")}/>
+                    <Form.Text className="text-muted">
+                        If the mission of your survey is clear to a user, they will have a better experience taking it.
+                    </Form.Text>
+                </Form.Group>
+
+                <Row>
+                    <Col>
+                        <Form.Group controlId="surveyStartDate">
+                            <Form.Label>Start Date</Form.Label>
+                            <Form.Control type="date" value={start_date} onChange={handleInputChange("start_date")}/>
+                            <Form.Text className="text-muted">
+                                The date your survey starts taking submissions.
+                            </Form.Text>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="surveyEndDate">
+                            <Form.Label>End Date</Form.Label>
+                            <Form.Control type="date" value={end_date} onChange={handleInputChange("end_date")}/>
+                            <Form.Text className="text-muted">
+                                The date your survey closes for submissions.
+                            </Form.Text>
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Form.Group controlId="securedStatus">
+                    <Form.Check type="checkbox"  label="Make Survey Public" />
+                </Form.Group>
+
+                <Button variant="success" type="submit" onClick={createSurvey}>
+                    Create Survey
+                </Button>
+            </Form>
+        )
+    }
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col>
+                    {basicDataFormInput()}
+                </Col>
+                <Col>
+                    questions
+                </Col>
+            </Row>
+
+            <br/>{title}<br/>{description}<br/>{secured ? "private": "public"}<br/>{start_date}<br/>{end_date}
+        </Container>
     )
 }
 
