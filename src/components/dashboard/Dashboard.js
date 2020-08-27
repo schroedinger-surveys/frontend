82edcb7a56b1854a96dbdbe00cbe4746bf8aa7a5
larapollehn <lara.pollehn@googlemail.com>
2020-08-27 13:22:53 +0200

    Added a loadingScreen with pure css spinner to the dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -13,6 +13,7 @@ import SideMenu from "../menu/SideMenu";
 import CreateSurveyButton from "./CreateSurveyButton";
 import {privateSurveyCount, publicSurveyCount} from "../utils/CountFunctions";
 import {setOverallCount} from "../../redux/actions/SurveyCount";
+import LoadingScreen from "../utils/LoadingScreen";
 
 /**
  * User Dashboard containing multiple elements to give user an overview of his acocunt
@@ -29,11 +30,15 @@ const Dashboard = () => {
     const counts = useSelector(state => state.SurveyCountReducer) // Gets the count of the surveys from the redux store
     const dispatch = useDispatch();
 
+    const [loading, setLoading] = useState(false);
+
     const getSurveyCounts = async () => {
         try {
+            setLoading(true);
             const privateSurveys = await privateSurveyCount();
             const publicSurveys = await publicSurveyCount();
             dispatch(setOverallCount(privateSurveys + publicSurveys)); // Sets the count of overall survey belonging to the user
+            setLoading(false);
         } catch (e) {
             log.error(e);
         }
@@ -45,35 +50,40 @@ const Dashboard = () => {
 
     return (
         <Container fluid>
-            <Row>
-                <Col xs={2}>
-                    <SideMenu/>
-                </Col>
-                <Col xs={4}>
-                    <Row>
-                        <Profile/>
-                    </Row>
-                    <Row>
+            {loading && (
+                <LoadingScreen/>
+            )}
+            {!loading && (
+                <Row>
+                    <Col xs={2}>
+                        <SideMenu/>
+                    </Col>
+                    <Col xs={4}>
+                        <Row>
+                            <Profile/>
+                        </Row>
+                        <Row>
+                            {counts.overallSurveys > 0 && (
+                                <SurveyList/>
+                            )}
+                            {counts.overallSurveys === 0 && (
+                                <UserPrompt size={"small"}/>
+                            )}
+                        </Row>
+                        <Row>
+                            <CreateSurveyButton/>
+                        </Row>
+                    </Col>
+                    <Col xs={6}>
                         {counts.overallSurveys > 0 && (
-                            <SurveyList/>
+                            <SurveySpotlight/>
                         )}
                         {counts.overallSurveys === 0 && (
-                            <UserPrompt size={"small"}/>
+                            <UserPrompt size={"large"}/>
                         )}
-                    </Row>
-                    <Row>
-                        <CreateSurveyButton/>
-                    </Row>
-                </Col>
-                <Col xs={6}>
-                    {counts.overallSurveys > 0 && (
-                        <SurveySpotlight/>
-                    )}
-                    {counts.overallSurveys === 0 && (
-                        <UserPrompt size={"large"}/>
-                    )}
-                </Col>
-            </Row>
+                    </Col>
+                </Row>
+            )}
         </Container>
     )
 }
