77539020ef312804c11452a6ff4965e84d70466f
larapollehn <lara.pollehn@googlemail.com>
2020-09-19 14:07:22 +0200

    Changed implementation of matchSubmissionCount based on the usage of Cache

--- src/components/submissions/Submissions.js
+++ src/components/submissions/Submissions.js
@@ -41,20 +41,19 @@ const Submissions = () => {
      * default is false (setup), in Pagination it is called with true
      * @returns {Promise<void>}
      */
-    const setupPrivateSurveys = async (page_number = 0 , pagination = false) => {
+    const setupPrivateSurveys = async (page_number = 0, pagination = false) => {
         if (privateSurveys.length === 0 || pagination) {
             setLoading(true);
 
             const privateSurveyList = await SurveyAPIHandler.cacheMiddleware(() => SurveyAPIHandler.surveyPrivateGet(page_number, itemsPerPage), "privateSurveys");
-
-            if(!pagination){
+            console.log(privateSurveyList);
+            if (!pagination) {
                 const privateSurveyCounts = await SurveyAPIHandler.cacheMiddleware(SurveyAPIHandler.privateSurveyCount, "privateSurveyCount");
                 setPrivateSurveysCount(privateSurveyCounts);
             }
 
             const matchedSurveys = await matchSubmissionCount(privateSurveyList);
             setPrivateSurveys(matchedSurveys);
-
             setLoading(false);
         }
         setDisplayPublic(false);
@@ -74,7 +73,7 @@ const Submissions = () => {
 
             const publicSurveyList = await SurveyAPIHandler.cacheMiddleware(() => SurveyAPIHandler.surveyPublicGet(page_number, itemsPerPage), "publicSurveys");
 
-            if (!pagination){
+            if (!pagination) {
                 const publicSurveysCounts = await SurveyAPIHandler.cacheMiddleware(SurveyAPIHandler.publicSurveyCount, "publicSurveyCount");
                 setPublicSurveyCount(publicSurveysCounts);
             }
@@ -97,13 +96,12 @@ const Submissions = () => {
     const matchSubmissionCount = async (surveys) => {
         const tempSurveyList = [];
         for (let i = 0; i < surveys.length; i++) {
-            const apiResponse = await SubmissionAPIHandler.submissionCount(surveys[i].id)
-            if (apiResponse.status === 200) {
-                tempSurveyList.push({
-                    survey: surveys[i],
-                    submissionCount: apiResponse.data.count
-                })
-            }
+            const apiResponse = await SubmissionAPIHandler.cacheMiddleware(() => SubmissionAPIHandler.submissionCount(surveys[i].id), "submissions", surveys[i].id)
+            tempSurveyList.push({
+                survey: surveys[i],
+                submissionCount: apiResponse
+            })
+
         }
         return tempSurveyList;
     }
@@ -169,8 +167,22 @@ const Submissions = () => {
                 {choseSurvey && redirectToSpotlight(selectedSurvey)}
                 <Col xs={3} style={{marginTop: "30px"}}>
                     <h3>Choose type of survey</h3>
-                    <button style={{borderRadius: "8px", border: "none", backgroundColor: "orange", marginRight: "5px", color: "white"}} onClick={() => setupPrivateSurveys()}>Private</button>
-                    <button style={{borderRadius: "8px", border: "none", backgroundColor: "orange", marginRight: "5px", color: "white"}} onClick={() => setupPublicSurveys()}>Public</button>
+                    <button style={{
+                        borderRadius: "8px",
+                        border: "none",
+                        backgroundColor: "orange",
+                        marginRight: "5px",
+                        color: "white"
+                    }} onClick={() => setupPrivateSurveys()}>Private
+                    </button>
+                    <button style={{
+                        borderRadius: "8px",
+                        border: "none",
+                        backgroundColor: "orange",
+                        marginRight: "5px",
+                        color: "white"
+                    }} onClick={() => setupPublicSurveys()}>Public
+                    </button>
                 </Col>
                 <Col xs={8} style={{marginTop: "30px"}}>
                     List of survey with chosen type here
