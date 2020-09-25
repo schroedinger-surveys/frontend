347fe8fd54aadb56d6f43cdf1431b4c7635b1fb0
larapollehn <lara.pollehn@googlemail.com>
2020-09-25 13:17:45 +0200

    EaSSEN MCHEN

--- src/components/utils/SurveyStatus.js
+++ src/components/utils/SurveyStatus.js
@@ -5,11 +5,11 @@ export const getCurrentStatus = (start_date, end_date) => {
     const startDate = new Date(start_date).getTime();
     const endDate = new Date(end_date).getTime();
     if (startDate > today) {
-        return <span style={{color: "orange"}}>pending</span>
+        return <span style={{color: "orange"}}>p</span>
     } else if (endDate < today) {
-        return <span style={{color: "darkred"}}>closed</span>
+        return <span style={{color: "darkred"}}>c</span>
     } else if (startDate <= today && endDate >= today) {
-        return <span style={{color: "darkgreen"}}>active</span>
+        return <span style={{color: "darkgreen"}}>a</span>
     }
 }
 
