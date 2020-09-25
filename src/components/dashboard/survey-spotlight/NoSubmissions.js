eed375de46ecf8e6b75881e7efaa56c29e0ea135
larapollehn <lara.pollehn@googlemail.com>
2020-09-25 21:02:25 +0200

    Questions font color

--- src/components/dashboard/survey-spotlight/NoSubmissions.js
+++ src/components/dashboard/survey-spotlight/NoSubmissions.js
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from "react";
 
-import sleepingKitty from "./icons/kitty.png";
-import sadKitty from "./icons/mad_kitty.png";
-import wonderingKitty from "./icons/cat.png";
+import emptyBox from "./icons/box.png";
 import {connect} from "react-redux";
 
 const NoSubmissions = (props) => {
@@ -14,11 +12,11 @@ const NoSubmissions = (props) => {
         const endDate = new Date(props.selectedSurvey.end_date).getTime();
 
         if (startDate > today) {
-            return {img_src: sleepingKitty, message: "your survey is not active yet.", instructions: "You need to wait until "+props.selectedSurvey.start_date};
+            return {message: "your survey is not active yet.", instructions: "You need to wait until "+props.selectedSurvey.start_date};
         } else if (endDate < today) {
-            return {img_src: sadKitty, message: "your survey is already closed and no submission were submitted.", instructions: "You could create the survey again and share it a little more."};
+            return {message: "your survey is already closed and no submission were submitted.", instructions: "You could create the survey again and share it a little more."};
         } else if (startDate <= today && endDate >= today) {
-            return {img_src: wonderingKitty, message: "you maybe did not share your Survey?", instructions: "Start sharing your survey by creating and sharing Share-Links"};
+            return {message: "you maybe did not share your Survey?", instructions: "Start sharing your survey by creating and sharing Share-Links"};
         }
     }
 
@@ -29,9 +27,8 @@ const NoSubmissions = (props) => {
     return(
         <div>
             <h3>No Submissions because {reason.message}</h3>
-            <img src={reason.img_src} style={{width: "200px"}} alt={""}/>
+            <img className={"no_submissions-image"} src={emptyBox} alt={"No Surveys yet, empty box of Schroedinger"}/>
             <p>What now? - {reason.instructions}</p>
-
         </div>
     )
 }
