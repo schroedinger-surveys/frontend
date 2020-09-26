54f5f5f2ba28bbe0805dadd44f7794000253b1cf
larapollehn <lara.pollehn@googlemail.com>
2020-09-27 00:22:08 +0200

    Buttonin CreateSurvey

--- src/components/survey/CreateSurvey.js
+++ src/components/survey/CreateSurvey.js
@@ -101,7 +101,14 @@ const CreateSurvey = () => {
                         </Col>
                     ))}
                 </Row>
-                <Button variant={"light"} onClick={addConstrainedOption}>Add option</Button>
+                <button className={"add_option_btn"} onClick={addConstrainedOption}>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-plus-square" fill="currentColor"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd"
+                              d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                        <path fillRule="evenodd"
+                              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg></button>
             </div>
         )
     }
@@ -111,7 +118,8 @@ const CreateSurvey = () => {
      * by adding an object to the constrainedOptions array
      * and incrementing the optionsIndex
      */
-    const addConstrainedOption = () => {
+    const addConstrainedOption = (event) => {
+        event.preventDefault();
         const currentOptions = constrainedOptions;
         currentOptions.push({number: optionsIndex});
         setConstrainedOptions(currentOptions);
@@ -203,27 +211,25 @@ const CreateSurvey = () => {
         return (
             <Form>
                 <BasicForm params={params}/>
-
+                <hr/>
                 <Row>
                     <Col>
                         {constrainedQuestion()}
-                        <Button variant="warning" onClick={addConstrainedQuestion}
-                                style={{marginBottom: "15px", color: "white"}}>Add Constrained Question</Button>
+                        <button className={"add_question_btn"} onClick={addConstrainedQuestion}>Add Constrained Question</button>
                     </Col>
                     <Col>
                         {freestyleQuestion()}
-                        <Button variant="warning" onClick={addFreestyleQuestion}
-                                style={{marginBottom: "15px", color: "white"}}>Add Freestyle Question</Button>
+                        <button className={"add_question_btn"} onClick={addFreestyleQuestion}>Add Freestyle Question</button>
                     </Col>
                 </Row>
-
+                <hr/>
                 <Form.Group>
                     <Form.Check id={"securedStatus"} type="checkbox" label="Make Survey Private"/>
                 </Form.Group>
 
-                <Button variant="success" type="submit" onClick={createNewSurvey}>
+                <button className={"submit_create_survey_button"} type="submit" onClick={createNewSurvey}>
                     Create Survey
-                </Button>
+                </button>
             </Form>
         )
     }
