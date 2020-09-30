419dac4a3bcff6a65fa345f889bda3f8172c4035
larapollehn <lara.pollehn@googlemail.com>
2020-09-30 07:33:33 -0700

    SurveyForms

--- src/components/survey/survey-submission/ClosedSurvey.js
+++ src/components/survey/survey-submission/ClosedSurvey.js
@@ -1,24 +1,13 @@
 import React from "react";
-
-import madCat from "../icons/mad_kitty.png"
-import boxLogo from "../../menu/icons/open-box.png";
+import {EuropeanTime} from "../../utils/TimeConverter";
 
 export const ClosedSurvey = (survey) => {
-    return(
-        <div className={"survey_wrapper"}>
-            <div className={"survey_wrapper_logo"}>
-                <a href={"https://schroedinger-survey.de"}>
-                    <img className={"box_logo survey_logo"} src={boxLogo} alt={"schroedingers survey cat box"}/>
-                </a>
-                Schrödinger Survey
-            </div>
-            <div id={"app_page_body"}>
-                <div style={{textAlign: "center"}}>
-                    <h2 style={{color: "darkred", fontSize: "50px"}}>Closed Survey</h2>
-                    <img src={madCat} alt={"sleeping cat"}/>
-                    <p>This survey is already closed, you can therefore not see the survey or submit your answers.</p>
-                    <p>Survey was closed at <span style={{fontWeight: "bolder"}}>{survey.end_date}!</span></p>
-                </div>
+    return (
+        <div id={"app_page_body"}>
+            <div style={{textAlign: "center", marginTop: "20px"}}>
+                <h2 style={{color: "darkred", fontSize: "50px"}}>Closed Survey</h2>
+                <p>This survey is already closed, you can therefore not see the survey or submit your answers.</p>
+                <p>Survey was closed <span style={{fontWeight: "bolder"}}>{EuropeanTime(survey.end_date)}!</span></p>
             </div>
         </div>
     )
--- src/components/survey/survey-submission/PendingSurvey.js
+++ src/components/survey/survey-submission/PendingSurvey.js
@@ -2,24 +2,14 @@ import React from "react";
 
 import sleepingCat from "../icons/kitty.png"
 import boxLogo from "../../menu/icons/open-box.png";
+import {EuropeanTime} from "../../utils/TimeConverter";
 
 export const PendingSurvey = (survey) => {
-    return(
-        <div className={"survey_wrapper"}>
-            <div className={"survey_wrapper_logo"}>
-                <a href={"https://schroedinger-survey.de"}>
-                    <img className={"box_logo survey_logo"} src={boxLogo} alt={"schroedingers survey cat box"}/>
-                </a>
-                Schrödinger Survey
-            </div>
-            <div id={"app_page_body"}>
-                <div style={{textAlign: "center"}}>
-                    <h2 style={{color: "orange", fontSize: "50px"}}>Inactive Survey</h2>
-                    <img src={sleepingCat} alt={"sleeping cat"}/>
-                    <p>This survey is not active yet, you can therefore not see the survey or submit your answers.</p>
-                    <p>Come back at <span style={{fontWeight: "bolder"}}>{survey.start_date}!</span></p>
-                </div>
-            </div>
+    return (
+        <div style={{textAlign: "center"}}>
+            <h2 style={{color: "orange", fontSize: "50px", marginTop: "20px"}}>Inactive Survey</h2>
+            <p>This survey is not active yet, you can therefore not see the survey or submit your answers.</p>
+            <p>Come back <span style={{fontWeight: "bolder"}}>{EuropeanTime(survey.start_date)}!</span></p>
         </div>
     )
 }
