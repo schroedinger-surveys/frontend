96b21543f88b1c36340820252e4e14348dc64858
larapollehn <lara.pollehn@googlemail.com>
2020-09-30 04:12:30 -0700

    Fixed viewport meta-tag and invalid nesting in SurveySpotlight

--- src/components/dashboard/survey-spotlight/SurveySpotlight.js
+++ src/components/dashboard/survey-spotlight/SurveySpotlight.js
@@ -65,7 +65,7 @@ const SurveySpotlight = (props) => {
                 {(getSurveyStatus(props.selectedSurvey.start_date, props.selectedSurvey.end_date) === "pending" ||
                     (getSurveyStatus(props.selectedSurvey.start_date, props.selectedSurvey.end_date) === "active" && submissionCount === 0)) &&
                 <Tab className={"survey_spotlight_tab-item"} eventKey="edit" title={
-                    <a onClick={() => setRedirect(true)}>
+                    <button className={"edit_survey_button_tab"} onClick={() => setRedirect(true)}>
                     <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-pencil-square" fill="currentColor"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -74,7 +74,7 @@ const SurveySpotlight = (props) => {
                               d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                     </svg>
                     Edit Survey
-                </a>
+                </button>
                 }>
                 </Tab>
                 }
